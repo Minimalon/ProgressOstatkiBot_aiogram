8df318ead8d553edcf29971264a483f6ccc912c1
Minimalon <grafxartyr@gmail.com>
2023-05-06 12:15:53 +0300

    debug

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ async def start():
 
     bot = Bot(token=config.token, parse_mode='HTML')
     await get_commands(bot)
-    storage = RedisStorage.from_url(config.redisStorage)
-    dp = Dispatcher(storage=storage)
+    # storage = RedisStorage.from_url(config.redisStorage)
+    # dp = Dispatcher(storage=storage)
+    dp = Dispatcher()
 
     # Калбэки с прошлого бота для регистрации
     dp.callback_query.register(basic.get_start_callback, F.data == 'cb_last_ostatki')
